77e35705d1624886473acec931dd084e57ca0d77
anto__dev <antonidwm@gmail.com>
2023-08-05 14:26:45 +0100

    ADD  first section

--- src/pages/StylePB.js
+++ src/pages/StylePB.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
+import FirstSection from "../components/FirstSection";
 
+//import image
+
+import BGFirstSection from ''
 
 function StylePB() {
   return (
     <>
-    
-    <div>StylePB</div>
+      <FirstSection
+        BGImg={BGFirstSection}
+        title1={allTitle[0]}
+        txt1={allTxt[0]}
+        span1={allTxt[1]}
+        btnSwitch={true}
+        nameBtn={"Voir les infos"}
+        linkBtn={"#infoContent"}
+        ClassColorBtn={"sect__btn sect__btn--yellow"}
+        linkHrefbtn={true}
+        h2Switch={true}
+        h2Title={allTitle[1]}
+        bgBlack={true}
+      />
     </>
-  )
+  );
 }
 
-export default StylePB
+export default StylePB;
