dca27a89c8a27425553fd0484beb7f565f573b58
anto__dev <antonidwm@gmail.com>
2023-08-16 14:41:35 +0100

    ADD function go top

--- src/components/BoxMessageEnd.js
+++ src/components/BoxMessageEnd.js
@@ -3,6 +3,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function BoxMessageEnd() {
+
+  function goTop(){
+    window.scrollTo(0,0)
+  }
   return (
     <>
       <motion.div
@@ -16,7 +20,7 @@ function BoxMessageEnd() {
           duration:0.6
         }}
       >
-        <Link to="/styles" className="sect__title sect__title--linkJaune sect__title--2 sect__title--jaune">
+        <Link to="/styles" className="sect__title sect__title--linkJaune sect__title--2 sect__title--jaune" onClick={goTop} aria-label="Lien pour changer de page">
           Go explorer d'autres styles ?
         </Link>
       </motion.div>
