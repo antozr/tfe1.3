ac0172d2b495a7ad07ea694e17fce906aa95535e
anto__dev <antonidwm@gmail.com>
2023-08-19 01:55:29 +0100

    Patch reverse design

--- src/components/InterSectionBox.js
+++ src/components/InterSectionBox.js
@@ -12,7 +12,8 @@ function InterSectionBox({ WhitThext, backImg, textTitle, horiAnim }) {
       <div
         className="sect__nameBigBox sect__nameBigBox--fondImg sect--snapChild"
         style={backImg}
-        onMouseMove={horiAnim === true ? getTopSize : console.log('hey')}
+        onMouseMove={horiAnim === true ? getTopSize : console.log("hey")}
+        id={horiAnim === true ? "moveInter" : ""}
       >
         <h2 className="sect__title sect_title--2 sect__title--big sect__title--druzokh">
           {textTitle}
@@ -21,18 +22,27 @@ function InterSectionBox({ WhitThext, backImg, textTitle, horiAnim }) {
     );
   }
 
-  function getTopSize() {
+  function getTopSize(e) {
     let box1 = document.querySelector(".sect__artiste");
-    console.log(box1.offsetTop);
+    let box2 = document.querySelector("#moveInter");
+    console.log(e);
     let of1 = box1.offsetTop;
-    console.log(of1);
-    setTimeout(()=>{
+    if (e.screenY < ((window.innerHeight / 2)+100)) {
+      setTimeout(() => {
         window.scrollTo({
-            top: of1,
-            left: 0,
-            behavior: "smooth",
-          });
-    },800)
+          top: of1,
+          left: 0,
+          behavior: "smooth",
+        });
+      }, 400);
+    } else {
+      window.scrollTo({
+        top: box2.offsetTop,
+        left: 0,
+        behavior: "smooth",
+      });
+      console.log(box2);
+    }
   }
 
   return <>{WhitThext === true ? textBoxOn() : noTextBox()}</>;
